5b34ef3e86481a4f3f0ae6e5ba1cbd4908f3eaa7
Lucas Garcia <lucks.garcialg@gmail.com.br>
2022-12-22 22:13:45 -0300

    WIP Aula 3 - min 48

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,23 @@
+describe('Count recipients notifications', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova socilitação de amizade!'),
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+});
